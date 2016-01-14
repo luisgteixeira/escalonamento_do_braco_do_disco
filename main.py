1875f69513089d232266c7dd126f242fc715c5be
Luis Guilherme <luisguilherme.ufpi@gmail.com>
2016-01-14 09:58:52 -0300

    Leitura de arquivo modificada.

--- main.py
+++ main.py
@@ -6,28 +6,17 @@ from elevator import *
 from sstf import *
 from fcfs import *
 
-def main(argv):
-    # Recebe o nome do arquivo de entrada
-    inputfile = sys.argv[-1]
+def main():
+    # Carrega o arquivo em uma lista
+    lines = sys.stdin.readlines()
 
-    if(inputfile == sys.argv[0]): # Verifica se algum arquivo foi recebido
-        print('Arquivo nao pode ser lido!!')
-    else:
-        # Abre o arquivo
-        arq = open(inputfile, 'r')
-        # Carrega o arquivo em uma lista
-        lines = arq.readlines()
+    sstf = SSTF(lines)
+    fcfs = FCFS(lines)
+    elevator = Elevator(lines)
 
-        sstf = SSTF(lines)
-        fcfs = FCFS(lines)
-        elevator = Elevator(lines)
-
-        print('FCFS', fcfs.output)
-        print('SSTF', sstf.output)
-        print('ELEVADOR', elevator.output)
-
-        # Fecha o arquivo
-        arq.close()
+    print('FCFS', fcfs.output)
+    print('SSTF', sstf.output)
+    print('ELEVADOR', elevator.output)
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
